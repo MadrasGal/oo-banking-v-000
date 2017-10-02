ca25a1f468c0f568a9126e9468af03586a321cdb
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 03:40:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,4 +8,34 @@ attr_accessor :sender, :receiver, :amount, :status
     @status = 'pending'
     @amount = amount
   end
+
+  def both_valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    reject_transfer
+    if status == "pending"
+      sender.deposit(0 - amount)
+      receiver.deposit(amount)
+      @status = "complete"
+      else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    if !sender.valid? || sender.balance - amount <= 0
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if status == "complete"
+      sender.deposit(amount)
+      receiver.deposit(0 - amount)
+      @status = "reversed"
+    end
+  end
 end
