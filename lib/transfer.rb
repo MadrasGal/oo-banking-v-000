dfdd05e78fc70a6432b05780b115ca72f0401774
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 03:37:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
-    
+    @receiver = receiver
     @status = 'pending'
     @amount = amount
   end
