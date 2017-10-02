0e5ef1126e8fc6659f1004c077e5872d7e7f65f0
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 03:39:28 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,19 +9,20 @@ class BankAccount
     @status = 'open'
   end
 
-  def deposit
+  def deposit(amount)
+    @balance += amount
 
   end
 
   def display_balance
-    @balance
+    "Your balance is $#{balance}."
   end
 
   def valid?
-
+    status == "open" && balance > 0
   end
 
   def close_account
-    self.clear_all
+    @status = "closed"
   end
 end
