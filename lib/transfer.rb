324f0b437c4ff66126a80e93d56d0570a823ab28
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 03:36:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount)
     @sender = sender
-    @reciever = BankAccount.new(reciever)
+    
     @status = 'pending'
     @amount = amount
   end
